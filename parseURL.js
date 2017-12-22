0012607968dff15782f3f8644fce252d5ae378b4
Schreibmann <jewishreich@gmail.com>
2017-12-22 22:46:50 +0300

    gh-pages-test

--- parseURL.js
+++
@@ -1,7 +0,0 @@
- parseURL = (url) => {
-  let a = document.createElement( "a" );
-  a.href = url;
-  console.log( `Port: ${a.port}` ) ;
-};
-
-parseURL('http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo');
